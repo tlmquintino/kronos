2ae3f28c7f1d6dca8216a4e5229ed1c269dbb5a9
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-07-15 14:02:12 +0100

    mkl support for multi-threaded

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -8,11 +8,17 @@
 option( MKL_PARALLEL "if mkl shoudl be parallel" OFF )
 
 if( MKL_PARALLEL )
+
   set( __mkl_lib_par  MKL_LIB_INTEL_THREAD )
   set( __mkl_lib_name mkl_intel_thread )
+
+  find_package(Threads)
+
 else()
+
   set( __mkl_lib_par MKL_LIB_SEQUENTIAL )
   set( __mkl_lib_name mkl_sequential )
+
 endif()
 
 
@@ -32,19 +38,25 @@ if( MKL_INCLUDE_DIR ) # use include dir to find libs
     set( MKL_INCLUDE_DIRS ${MKL_INCLUDE_DIR} )
 
 	if( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-	  get_filename_component( MKL_LIB_PATH ${MKL_INCLUDE_DIR}/../lib/intel64 ABSOLUTE )
-	  set( __libsfx _lp64 )
+      get_filename_component( MKL_LIB_PATH ${MKL_INCLUDE_DIR}/../lib/intel64 ABSOLUTE )
+      set( __libsfx _lp64 )
 	else()
 	  get_filename_component( MKL_LIB_PATH ${MKL_INCLUDE_DIR}/../lib/ia32 ABSOLUTE )
-	  set( __libsfx "" )
+      set( __libsfx "" )
 	endif()
+
+    message( STATUS "ICC_LIB_PATH ${ICC_LIB_PATH}" )
 
     find_library( MKL_LIB_INTEL         NAMES mkl_intel${__libsfx} PATHS ${MKL_LIB_PATH} )
     find_library( ${__mkl_lib_par}      NAMES ${__mkl_lib_name} PATHS ${MKL_LIB_PATH} )
     find_library( MKL_LIB_CORE          NAMES mkl_core PATHS ${MKL_LIB_PATH} )
 
+    if( MKL_PARALLEL )
+        find_library( MKL_LIB_IOMP5  NAMES iomp5 PATHS ${MKL_LIB_PATH} )
+    endif()
+
     if( MKL_LIB_INTEL AND ${__mkl_lib_par} AND MKL_LIB_CORE )
-        set( MKL_LIBRARIES ${MKL_LIB_INTEL} ${${__mkl_lib_par}} ${MKL_LIB_CORE} )
+        set( MKL_LIBRARIES ${MKL_LIB_INTEL} ${${__mkl_lib_par}} ${MKL_LIB_CORE} ${MKL_LIB_IOMP5} ${CMAKE_THREAD_LIBS_INIT} )
     endif()
 
 endif()
